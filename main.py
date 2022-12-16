4c1d25cc948e0ed0187c21a0a01ef729e88520d8
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-12-15 19:28:27 -0500

    Update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import sqlite3
 import os
 
-
 app = Flask(__name__)
 
 def get_db_connection():
@@ -26,7 +25,7 @@ def bootstrap():
     patientListSql = conn.execute('SELECT * FROM patient_table').fetchall()
     conn.close()
     print('patientListSql:', patientListSql)
-    return render_template('bootstrap_example.html', listPatients=patientListSql) 
+    return render_template('patients.html', listPatients=patientListSql) 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
